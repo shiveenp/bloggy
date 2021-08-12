0d6d8d8d059401abd12cba58a8ad9c0122c0736d
Shiveen Pandita <spandita@atlassian.com>
2021-08-12 18:19:34 +1000

    Added social image generator

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,17 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight"),
     markdownIt = require('markdown-it');
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const generateSocialImages = require("@manustays/eleventy-plugin-generate-social-images");
 module.exports = eleventyConfig => {
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(generateSocialImages, {
+    promoImage: "./assets/images/about_me.jpg",
+    outputDir: "./_site/img/preview",
+    urlPath: "/img/preview",
+    siteName: "shiveenp.com/",
+    titleColor: "#fedb8b"
+  });
   eleventyConfig.addLiquidFilter("dateToRfc3339", pluginRss.dateRfc3339);
   eleventyConfig.addLiquidFilter("getNewestCollectionItemDate", pluginRss.getNewestCollectionItemDate);
   eleventyConfig.addLiquidFilter("absoluteUrl", pluginRss.absoluteUrl);
